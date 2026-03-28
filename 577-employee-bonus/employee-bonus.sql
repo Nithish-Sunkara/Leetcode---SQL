549320f54853833cfc4a38157d3590ff3014ee97
Nithish Sunkara <nithishsunkara.ns@gmail.com>
2026-03-28 00:03:17 -0400

    Time: 849 ms (93.83%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,6 +1,5 @@
-# Write your MySQL query statement below
-SELECT name, bonus
-FROM Employee
-left JOIN Bonus
-ON Bonus.empId = Employee.empId
-WHERE bonus IS NULL OR bonus < 1000;
+SELECT e.name, b.Bonus
+FROM Employee e
+LEFT JOIN Bonus b
+ON e.empId = b.empId
+WHERE bonus < 1000 or bonus IS NULL;
